62008d9f76cec123c5fcc75b974bcad88aaa7c6a
Ajsanna <ajsanna@yahoo.com>
2025-11-16 11:13:12 -0800

    Struct complete, logfs_open complete. Next step is append

--- logfs.c
+++ logfs.c
@@ -10,9 +10,23 @@
 #include <pthread.h>
 #include "device.h"
 #include "logfs.h"
+#include "system.h"
+#include <stdlib.h>
 
 #define WCACHE_BLOCKS 32
 #define RCACHE_BLOCKS 256
+
+/*
+*   Edited by Alexander Sanna
+*   November 15th, 2025 
+*   Operating Systems Fall 2025 - Tony Givargis
+
+*   Description:
+*   IMplementation of key/value storage system on a raw block dev.
+*   Project was created and tested with a real device (30GB USB Flash)
+*   
+    *** IMPORTANT NOTE: Device used in testing has a 512kb block size. this is designed specifically for that.
+*/
 
 /**
  * Needs:
@@ -29,3 +43,57 @@
  */
 
 /* research the above Needed API and design accordingly */
+
+struct logfs{
+    uint64_t size;
+    uint64_t capacity; 
+    uint64_t head, tail;
+    void* wbuf_;
+    void* wbuf;
+    pthread_mutex_t lock; 
+    struct device* device;
+    uint64_t offset;
+    pthread_cond_t Data_avail;
+    pthread_cond_t Space_avail;
+    pthread_t worker;
+};
+
+struct logfs *logfs_open(const char *pathname)
+{
+    struct logfs * logfs = malloc(sizeof(struct logfs));
+    if(logfs == NULL){return NULL;}
+
+    logfs -> device = device_open(pathname);
+    if(logfs->device == NULL){return NULL;}
+
+    logfs -> capacity = device_block(logfs->device) * 10;
+
+    logfs -> size = 0;
+    logfs -> head = 0;
+    logfs -> tail = 0;
+    logfs->offset = 0;
+
+
+    logfs -> wbuf_ = malloc(logfs->capacity + page_size());
+    logfs->wbuf = memory_align(logfs->wbuf_, page_size());
+
+    pthread_create(&logfs->worker, NULL, worker_func, logfs);
+    pthread_mutex_init(&logfs->lock, NULL);
+    pthread_cond_init(&logfs -> Data_avail, NULL);
+    pthread_cond_init(&logfs -> Space_avail, NULL);
+
+
+    return logfs;
+}
+
+int logfs_append(struct logfs *logfs, const void *buf, uint64_t len)
+{
+    
+}
+int logfs_read(struct logfs *logfs, void *buf, uint64_t off, size_t len);
+
+void* worker_func(void* arg){
+    return 0; /*STUB*/
+}
+
+void logfs_close(struct logfs *logfs);
